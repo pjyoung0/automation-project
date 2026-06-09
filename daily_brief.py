4cc74bafde5537360bc00c8884b69e788cbfa75d
pjyoung0 <junyoung37590@yonsei.ac.kr>
2026-06-09 20:10:12 +0900

    Update daily_brief.py

--- daily_brief.py
+++ daily_brief.py
@@ -1,1 +1,16 @@
-print("Hello DS Asset Management")
+import os
+import google.generativeai as genai
+
+genai.configure(
+    api_key=os.environ["GEMINI_API_KEY"]
+)
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
+
+response = model.generate_content(
+    "삼성전자 투자포인트 3줄"
+)
+
+print(response.text)
